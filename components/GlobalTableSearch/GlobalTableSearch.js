5d9ba7192249bb2d59c5c72a00bb4efaffb8bf8a
“Quinn <quinnlashinsky@gmail.com>
2021-03-19 20:47:18 -0400

    Remove non-working usseAsyncDebounce hook. Remove unused yarn.lock file

--- components/GlobalTableSearch/GlobalTableSearch.js
+++ components/GlobalTableSearch/GlobalTableSearch.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
-import { useAsyncDebounce } from "react-table";
 
 function GlobalTableSearch({ filter, setFilter }) {
   const [value, setValue] = useState(filter);
-
-  // const onChange = useAsyncDebounce((value) => {
-  //   setFilter(value);
-  // }, 300);
 
   return (
     <div className="sm:text-2xl h-16 bg-phillies-maroon-retro min-w-full inline-flex justify-center items-center rounded-lg">
